40d8e95c2fcd11731f2eb6ce27a077771dd5f825
ossuchas <suchat.s14102526@gmail.com>
2019-12-20 10:31:22 +0700

    modified url https ap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://c57d87af.ngrok.io/api/v1'
+  // apiUrl: 'https://c57d87af.ngrok.io/api/v1'
+  apiUrl: 'https://apchatbotapi.apthai.com/api/v1'
 };
